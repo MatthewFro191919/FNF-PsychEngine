8ef2637aebd2127612025af7ca503449fd9d84dc
MatthewFro191919 <matthewfroncek4@gmail.com>
2025-08-30 12:01:44 -0400

    Update unix.sh

--- setup/unix.sh
+++ setup/unix.sh
@@ -9,10 +9,12 @@ echo Installing dependencies...
 echo This might take a few moments depending on your internet speed.
 haxelib install flixel 5.6.1
 haxelib install flixel-addons 3.2.2
-haxelib install flixel-tools 1.5.1
 haxelib install flixel-ui
+haxelib install flixel-tools 1.5.1
 haxelib install hscript-iris 1.1.3
 haxelib install tjson 1.4.0
+haxelib install json2object
+haxelib install hxjsonast
 haxelib install hxdiscord_rpc 1.2.4
 haxelib install hxvlc 2.0.1 --skip-dependencies
 haxelib install lime 8.1.2
